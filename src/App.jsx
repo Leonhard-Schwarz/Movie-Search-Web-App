4f11707873b9a4fe7ea39ba2e6f521c37fa1caac
Leonhard Schwarz <leo.schwarz.wangen@gmail.com>
2023-10-30 23:58:17 +0100

    updated OMDB API URL to use HTTPS

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import "./App.css";
 import SearchIcon from "./assets/search.svg";
 
-const API_URL = "http://www.omdbapi.com/?apikey=4e3bcfc4";
+const API_URL = "https://www.omdbapi.com/?apikey=4e3bcfc4";
 
 const App = () => {
   const [movies, setMovies] = useState([]);

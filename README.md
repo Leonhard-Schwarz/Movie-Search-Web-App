9927c481e9ceca0693e827d00494ede5d1dc5755
Leonhard Schwarz <leo.schwarz.wangen@gmail.com>
2023-10-30 23:32:52 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Movie Search Web App 🎬
+
+This project is a movie search application built using React. Users can search for movies by entering a query, and the app will display the search results from the OMDB API. The app includes infinite scrolling functionality to load more search results as the user scrolls. Additionally, the app offers a light and dark mode based on user preferences.
+
+## Features
+
+- Search for movies by entering a search term.
+- Infinite scrolling for loading additional movie search results.
+- Clicking the search icon or pressing "Enter" triggers the search.
+- Light and dark mode based on user preferences.
+
+## Demo
+
+Check out the [Movie Search Web App Demo](https://leonhard-schwarz.github.io/Movie-Search-Web-App) here!
+
+## Acknowledgment
+
+This project is inspired by a tutorial from JS Mastery. The original code and tutorial can be found at [JS Mastery - React JS for Beginners Tutorial](https://www.youtube.com/watch?v=b9eMGE7QtTk&list=PLJcy0uabn41qkORUEq8DQsp-MUrwjpi6b&index=14). I added the Infinite scrolling and pressing "Enter" to search functionality. I also changed the CSS to include the light and dark mode based on user preferences.
+
+## Resources
+
+- [React Documentation](https://reactjs.org/)
+- [OMDB API](http://www.omdbapi.com/)
+- [ChatGPT by OpenAI](https://openai.com/chatgpt)
---
+++ src/App.jsx
@@ -0,0 +1,107 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState, useRef } from "react";
+import MovieCard from "./MovieCard";
+import "./App.css";
+import SearchIcon from "./assets/search.svg";
+
+const API_URL = "http://www.omdbapi.com/?apikey=4e3bcfc4";
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [error, setError] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const containerRef = useRef(null);
+
+  const searchMovies = async (title, page) => {
+    try {
+      const response = await fetch(`${API_URL}&s=${title}&page=${page}`);
+      const data = await response.json();
+
+      if (data.Response === "True") {
+        if (page === 1) {
+          setMovies(data.Search);
+        } else {
+          setMovies((prevMovies) => [...prevMovies, ...data.Search]);
+        }
+        setTotalPages(Math.ceil(data.totalResults / 10)); // Assuming 10 results per page
+        setError(null);
+      } else {
+        setError(data.Error);
+      }
+    } catch (error) {
+      setError("An error occurred while fetching data.");
+    }
+  };
+
+  const handleSearch = () => {
+    setCurrentPage(1);
+    setMovies([]);
+    searchMovies(searchTerm, 1);
+  };
+
+  const loadMore = () => {
+    const nextPage = currentPage + 1;
+    if (nextPage <= totalPages) {
+      searchMovies(searchTerm, nextPage);
+      setCurrentPage(nextPage);
+    }
+  };
+
+  const handleScroll = () => {
+    if (
+      containerRef.current &&
+      containerRef.current.getBoundingClientRect().bottom <= window.innerHeight
+    ) {
+      loadMore();
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
+
+  useEffect(() => {
+    setError("Start by searching for movies.");
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [handleScroll]);
+
+  return (
+    <div className="app">
+      <h1>Movie Search App</h1>
+
+      <div className="search">
+        <input
+          placeholder="Search for Movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+        <img src={SearchIcon} alt="search" onClick={handleSearch} />
+      </div>
+
+      {error ? (
+        <div className="empty">
+          <h2>{error}</h2>
+        </div>
+      ) : (
+        <div className="container" ref={containerRef}>
+          {movies.map((movie, index) => (
+            <MovieCard key={index} movie={movie} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
